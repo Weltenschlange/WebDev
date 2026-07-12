5b761d60b822b131341dde755a5ba3f934b23929
Elias Kehne <welt.schlange@gmail.com>
2026-07-12 11:55:44 +0200

    get all projects api

--- Praktikum 10/gradleProject/app/src/main/java/test/HelloResource.java
+++ Praktikum 10/gradleProject/app/src/main/java/test/HelloResource.java
@@ -4,6 +4,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Map;
 
+import jakarta.json.Json;
+import jakarta.json.JsonArrayBuilder;
+import jakarta.json.JsonObject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
@@ -117,6 +120,76 @@ public class HelloResource {
         return rb.build();
 
     }
+
+@GET
+@Path("/Projects")
+@Produces(MediaType.APPLICATION_JSON)
+public Response Projects() {
+    try {
+        JsonArrayBuilder projectsArray = Json.createArrayBuilder();
+
+        ResultSet projectRS = db.query("SELECT * FROM \"Projekt\"");
+
+        while (projectRS.next()) {
+            int id = projectRS.getInt(1);
+            String title = projectRS.getNString(2);
+            String logo = projectRS.getNString(3);
+            String startdatum = projectRS.getNString(4);
+            String kurzbeschreibung = projectRS.getNString(5);
+
+            JsonArrayBuilder artefactsArray = Json.createArrayBuilder();
+
+            ResultSet artefactRS = db.query(
+                "SELECT a.*, pa.arbeits_zeit FROM \"Artefakt\" a " +
+                "JOIN \"Projekt_Artefakt\" pa ON a.id = pa.artefakt_id " +
+                "WHERE pa.projekt_id = " + id
+            );
+
+            while (artefactRS.next()) {
+                int artId = artefactRS.getInt(1);
+                String artTitle = artefactRS.getNString(2);
+                String artKurzbeschreibung = artefactRS.getNString(3);
+                int artAufgabenbereichsId = artefactRS.getInt(4);
+                int artZeitaufwand = artefactRS.getInt(5);
+                int projArtZeitaufwand = artefactRS.getInt(6);
+
+                JsonObject artefactObj = Json.createObjectBuilder()
+                        .add("id", artId)
+                        .add("titel", artTitle)
+                        .add("kurzbeschreibung", artKurzbeschreibung)
+                        .add("aufgabenbereichsId", artAufgabenbereichsId)
+                        .add("zeitaufwand", artZeitaufwand)
+                        .add("projektZeitaufwand", projArtZeitaufwand)
+                        .build();
+
+                artefactsArray.add(artefactObj);
+            }
+            artefactRS.close();
+
+            JsonObject projectObj = Json.createObjectBuilder()
+                    .add("id", id)
+                    .add("titel", title)
+                    .add("logo", logo)
+                    .add("startdatum", startdatum)
+                    .add("kurzbeschreibung", kurzbeschreibung)
+                    .add("artefakte", artefactsArray.build())
+                    .build();
+
+            projectsArray.add(projectObj);
+        }
+        projectRS.close();
+
+        String jsonResult = projectsArray.build().toString();
+
+        ResponseBuilder rb = Response.ok(jsonResult, MediaType.APPLICATION_JSON);
+        return rb.build();
+
+    } catch (Exception e) {
+        ResponseBuilder rb = Response.status(Response.Status.INTERNAL_SERVER_ERROR);
+        rb.entity("{\"error\": \"" + e.getMessage() + "\"}");
+        return rb.build();
+    }
+}
 
     public ResultSet min(String table, String column) throws SQLException{
         String query = "SELECT MIN(" + column + ") FROM \"" + table + "\"";
